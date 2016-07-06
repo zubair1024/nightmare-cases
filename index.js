7263249a79ba947a4ac05b47db7d4775e635822c
Zubair Ahmed <zubair.ahmed@roamworks.com>
2016-07-06 20:45:49 +0400

    clean up

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var Nightmare = require('nightmare');
 
 var testCase = new Nightmare({show: true, openDevTools: true,})
     .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-    .goto('http://latest.roamworks.com/actiondemo/caesar/#!/login')
+    .goto('url')
     .wait(1000)
-    .type('#username', 'AliKhan')
-    .type('#password', 'alikhan1234')
+    .type('#username', 'username')
+    .type('#password', 'password')
     .click('button.btn')
     .wait(3000)
     .screenshot('test.png')
